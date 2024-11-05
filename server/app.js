77130774609d33c306dc97116c33cc960d9c5500
Zelun Zhang <zelunzhangofficial@gmail.com>
2024-11-05 22:33:28 +1030

    Basic css is done.
    To-Do: some hover button effects. and some generating effect.

--- server/app.js
+++ server/app.js
@@ -32,32 +32,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/prompt', async (req, res) => {
-  // const result = await axios.post('https://modelslab.com/api/v6/realtime/text2img', 
-  // {
-  //   "key" : apikey,
-  //   "prompt": req.query.prompt,
-  //   "negative_prompt": "bad quality",
-  //   "width": "512",
-  //   "height": "512",
-  //   "safety_checker": false,
-  //   "seed": null,
-  //   "samples":1,
-  //   "base64":false,
-  //   "webhook": null,
-  //   "track_id": null
-  // },
-  // {
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   }
-  // })
-  // // // result.data.output[0] is the output link
-  // res.json({ output: result.data.output[0] });
+  const result = await axios.post('https://modelslab.com/api/v6/realtime/text2img', 
+  {
+    "key" : apikey,
+    "prompt": req.query.prompt,
+    "negative_prompt": "bad quality",
+    "width": "512",
+    "height": "512",
+    "safety_checker": false,
+    "seed": null,
+    "samples":1,
+    "base64":false,
+    "webhook": null,
+    "track_id": null
+  },
+  {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  // // result.data.output[0] is the output link
+  res.json({ output: result.data.output[0] });
 
-  
 
-  // testing because it has limited access to the API
-  res.json({ output: 'https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/generations/d6003dc0-be83-4514-a612-d001c9036c5f-0.png' });
+
+  // // testing because it has limited access to the API
+  // res.json({ output: 'https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/generations/d6003dc0-be83-4514-a612-d001c9036c5f-0.png' });
 })
 
 app.post('/save', async (req, res) => {
